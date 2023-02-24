2fc800ecf96809a23541805c138be5fdab0dceb5
bysophieya <https://github.com/bysophieya/yakovlevasofia.git>
2023-02-24 20:19:46 +0300

    lab1

--- lab1.py
+++ lab1.py
@@ -1,9 +1,12 @@
-mesto=input('Введите номер места в вагоне')
+mesto=int(input('Введите номер места в вагоне'))
+if mesto > 54:
+    print('Такого места нет')
+    exit()
 if (0 < mesto <= 36):
     print('Ваше место в купе')
 else:
     print('Ваше место боковое')
-if mesto % 2 = 0:
+if mesto % 2 == 0:
     print('верхнее')
 else:
     print('нижнее')
