7975e2b9679293ed7f5b91b295a8c49adba364e2
bysophieya <https://github.com/bysophieya/yakovlevasofia.git>
2023-03-04 16:41:48 +0300

    3

--- 2.2/main.py
+++ 2.2/main.py
@@ -1,6 +1,9 @@
-word= ''
-n = ''
-while word != 'стоп':
-    word = input()
-    n = n + word + ' '
-print(n)
+word=input('Введите слово ')
+k=0
+for i in range(0,len(word)):
+    if word[i] =='ф':
+        k= k+1
+if k >= 1:
+    print('Ого!Это редкое слово!')
+else:
+    print('Эх, это не очень редкое слово...')
