8c1f546bed330047c7042f8224a33460b5ce00e0
bysophieya <https://github.com/bysophieya/yakovlevasofia.git>
2023-03-04 17:19:13 +0300

    4

---
+++ 2.4/main.py
@@ -0,0 +1,17 @@
+c=0
+k=0
+k1=0
+for i in range(1,7):
+    for j in range(1,5):
+        print( i,"+",j,"= ",end="")
+        otvet = int(input())
+        c = i + j
+        if otvet == c:
+            print('Правильно!')
+            k+=1
+        else:
+            print('Ответ неверный.')
+            k1+=1
+        if k1==3:
+            print('Игра окончена.Правильных ответов:', k)
+            exit()
