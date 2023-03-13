25f6bf5b3eda9fd544ffe8526cf2a41036034797
bysophieya <https://github.com/bysophieya/yakovlevasofia.git>
2023-03-13 17:59:53 +0300

    1

---
+++ lab4/main.py
@@ -0,0 +1,9 @@
+def chislo(x):
+    if x % 3 == 0:
+        print('Число делится на 3')
+        return None
+    else:
+        print('Число не делится на 3')
+        return None
+number= int(input('Введите число '))
+chislo(number)
