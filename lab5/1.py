4ac9798755b4fdf1d45f2b807cba1b6d9259c8ec
bysophieya <https://github.com/bysophieya/yakovlevasofia.git>
2023-03-18 14:11:08 +0300

    1

---
+++ lab5/1.py
@@ -0,0 +1,6 @@
+spisok=[2,3,5,7,9]
+chislo=int(input('Введите число '))
+if chislo in spisok:
+    print(spisok,chislo,'Поздравляю, вы угадали число !')
+else:
+    print(spisok,chislo,'Нет такого числа ')
