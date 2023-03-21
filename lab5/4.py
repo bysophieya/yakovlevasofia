40837e7f61034aaed061e5304018a21a14fb8314
bysophieya <125361714+bysophieya@users.noreply.github.com>
2023-03-21 14:19:12 +0300

    Update 4.py

--- lab5/4.py
+++ lab5/4.py
@@ -2,12 +2,12 @@ import random
 k=0
 spisok=['Киселева',"Байол","Столярова","Емельянова","Киреева","Яковлева","Пискунова","Овчинникова","Чугунова","Зубцова"]
 spisok2=["Савинова","Гончарова","Иванов","Мельникова","Величко","Стрельцова","Терентьева","Гребенщиков","Шишкина","Овсянко"]
-team = tuple(random.sample(spisok, 5)+random.sample(spisok2, 5))
+team = tuple(random.sample(spisok, 5)+random.sample(spisok2, 5)) #выбираем по 5 рандомных людей из 2 списков
 print('Студенты моей группы ',spisok)
 print('Студенты другой группы ',spisok2)
 print('Спортивная команда ',team)
 print('Длина кортежа ',len(team))
-team = tuple(sorted(team))
+team = tuple(sorted(team)) #сортируем по алфавиту
 print('Отсортированный список ',team)
 if 'Иванов' in team:
     k+=1
