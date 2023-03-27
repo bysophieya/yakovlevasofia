febfe2630ccd24f28f2c8a95f98cb47b2eb9fe01
bysophieya <https://github.com/bysophieya/yakovlevasofia.git>
2023-03-27 22:46:42 +0300

    1

---
+++ lab6/1.py
@@ -0,0 +1,10 @@
+countries={"Россия":"Москва","Германия":"Берлин","Франция":"Париж","Норвегия":"Осло","Италия":"Рим"}
+for key in countries:
+    print(key,'-',countries[key])
+stoliza=input("Введите страну ")
+user=countries.get(stoliza)
+print(user)
+list_keys = list(countries.keys())
+list_keys.sort()
+for i in list_keys:
+    print(i, ':', countries[i])
