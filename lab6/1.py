2bfffe4263fe709c9a39a51ca7c94b51e7bc1ab7
bysophieya <125361714+bysophieya@users.noreply.github.com>
2023-03-28 13:59:21 +0300

    Update 1.py

--- lab6/1.py
+++ lab6/1.py
@@ -1,10 +1,10 @@
 countries={"Россия":"Москва","Германия":"Берлин","Франция":"Париж","Норвегия":"Осло","Италия":"Рим"}
 for key in countries:
-    print(key,'-',countries[key])
+    print(key,'-',countries[key]) #выводим на экран все пары ключ-значение
 stoliza=input("Введите страну:")
-user=countries.get(stoliza)
+user=countries.get(stoliza) # получаем столицу для страны
 print(user)
 list_keys = list(countries.keys())
-list_keys.sort()
+list_keys.sort() #сортируем страны по алфавиту
 for i in list_keys:
     print(i, ':', countries[i])
