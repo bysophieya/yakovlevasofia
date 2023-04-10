bf9b640ae209ae5011a46e30d402c50d86b93b70
bysophieya <https://github.com/bysophieya/yakovlevasofia.git>
2023-04-11 01:07:42 +0300

    8

--- lab8/2.py
+++ lab8/2.py
@@ -1,6 +1,5 @@
+import os
 holidays={"8 марта":'8марта.jpg',"14 февраля":'14февраля.jpg',"День рождения":'др.jpg',"Рождество":'рождество.jpg'}
 prazdnik=input('К какому празднику вы хотите открытку?')
-filename = holidays[prazdnik]
-with open(filename, 'rb') as f:
-    image = f.read()
-filename = image
+os.system(holidays[prazdnik]) #Функция os.system () выполняет команду, выводит любой вывод команды на консоль
+
---
+++ lab8/3.py
@@ -0,0 +1,21 @@
+from PIL import Image,ImageDraw,ImageFont
+import numpy as np
+from termcolor import colored
+
+img = Image.open('picture.jpg')
+img_arr = np.array(img) #Изображения преобразуются в массив Numpy в формате высоты, ширины, канала.
+
+img_arr[0: 400, 0: 200] = (0, 0, 0) #мы удалили бы пиксели в области от (0,0) до (400, 200) (верхняя левая сторона)
+img = Image.fromarray(img_arr) #создает изображение в памяти из объекта obj
+name=input('Кого вы хотите поздравить? ')
+draw_text = ImageDraw.Draw(img) #изображения для рисования
+myFont = ImageFont.truetype('Montserrat-Regular.ttf', 40) # подключаем шрифт
+draw_text.text(
+    (50,50), #расположение
+    '{},поздравляю!'.format(name),
+    font=myFont,
+    stroke_width=2,
+    fill=(0, 0, 255),
+    )
+img.show()
+img.save('newpicture.png')
