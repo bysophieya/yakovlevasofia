ed257bd7bdba02af0bf55906ab405e0eff99e856
bysophieya <https://github.com/bysophieya/yakovlevasofia.git>
2023-04-16 22:28:53 +0300

    8

--- lab7/3.py
+++ lab7/3.py
@@ -1,8 +1,7 @@
 from PIL import Image,ImageFilter
 images=['первый.jpg','второй.jpg','третий.jpg','четвертый.jpg','пятый.jpg']
 for img in images: #создаем цикл и пробегаемся по каждой фотке
-    with Image.open(img) as images:
-        image= Image.open(img)
-        img_f= image.filter(ImageFilter.FIND_EDGES) #накладываем фильтр выделения краев
-        img_f.show()
-        img_f.save('new'+img) #сохраняем с новым названием
+    image= Image.open(img)
+    img_f= image.filter(ImageFilter.FIND_EDGES) #накладываем фильтр выделения краев
+    img_f.show()
+    img_f.save('new'+img) #сохраняем с новым названием
