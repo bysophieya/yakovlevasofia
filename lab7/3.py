54997a93a7ae7f62654963140bd4e21739376869
bysophieya <https://github.com/bysophieya/yakovlevasofia.git>
2023-04-16 22:24:55 +0300

    8

--- lab7/3.py
+++ lab7/3.py
@@ -1,6 +1,8 @@
 from PIL import Image,ImageFilter
 images=['первый.jpg','второй.jpg','третий.jpg','четвертый.jpg','пятый.jpg']
 for img in images: #создаем цикл и пробегаемся по каждой фотке
-    image= Image.open(img)
-    img_f= image.filter(ImageFilter.FIND_EDGES) #накладываем фильтр выделения краев
-    img_f.save('filters/'+str(img)) #сохраняем с новым названием с помощью переменной img
+    with Image.open(img) as images:
+        image= Image.open(img)
+        img_f= image.filter(ImageFilter.FIND_EDGES) #накладываем фильтр выделения краев
+        img_f.show()
+        img_f.save('new'+img) #сохраняем с новым названием
--- lab7/4.py
+++ lab7/4.py
@@ -1,5 +1,6 @@
 from PIL import Image,ImageDraw,ImageFont
 img=Image.open('котик.jpg')
 z=Image.open('знак.png')
+z=z.reduce(7)
 img.paste(z,(0,0), mask=z) #Метод paste всталяет одно изображение в другое изображение  с координатами в угол,знак используется как маска
 img.show()
