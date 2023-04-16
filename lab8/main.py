fe9ed0f5d73aa1bd7b073a119a2c326b83bd4c1b
bysophieya <https://github.com/bysophieya/yakovlevasofia.git>
2023-04-16 22:11:37 +0300

    8

--- lab8/main.py
+++ lab8/main.py
@@ -1,10 +1,5 @@
 from PIL import Image
-import numpy as np
-
 img = Image.open('picture.jpg')
-img_arr = np.array(img) #Изображения преобразуются в массив Numpy в формате высоты, ширины, канала.
-
-img_arr[0: 400, 0: 200] = (0, 0, 0) #мы удалили бы пиксели в области от (0,0) до (400, 200) (верхняя левая сторона)
-img = Image.fromarray(img_arr)
-img.show()
-img.save('newpicture.jpg')
+img_crop=img.crop((0,30,564,591))
+img_crop.show()
+img_crop.save('newpicture.jpg')
