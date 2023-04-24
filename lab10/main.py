494421ee9a84d8693cb1339f712308f591e4c084
bysophieya <https://github.com/bysophieya/yakovlevasofia.git>
2023-04-24 23:26:15 +0300

    10

---
+++ lab10/main.py
@@ -0,0 +1,17 @@
+import json
+
+def print_product_info(product):
+    print(f"Название: {product['name']}")
+    print(f"Цена: {product['price']}")
+    print(f"Вес: {product['weight']}")
+    if product["available"]:
+        print("В наличии")
+    else:
+        print("Нет в наличии")
+    print() # выводим пустую строку для разделения продуктов
+with open("products.json", "r", encoding='utf-8') as f:
+    products2 = json.load(f) # загружаем данные из файла в переменную
+
+products = products2["products"] # получаем список продуктов из словаря products2
+
+list(map(print_product_info, products)) # применяем функцию print_product_info ко всем элементам списка products с помощью функции map
