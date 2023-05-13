184209f04a6925ae0914e015152a1acd4d609f3c
bysophieya <125361714+bysophieya@users.noreply.github.com>
2023-05-14 00:31:21 +0300

    Update main.py

--- laba11/main.py
+++ laba11/main.py
@@ -1,11 +1,11 @@
-class Restaurant:
-    def __init__(self,restaurant_name,cuisine_type):
+class Restaurant: 
+    def __init__(self,restaurant_name,cuisine_type): #содержит 2 атрибута
         self.restaurant_name=restaurant_name
         self.cuisine_type=cuisine_type
     def describe_restaurant(self):
         print('Название ресторана: ',self.restaurant_name, '\nКухня ресторана: ',self.cuisine_type)
     def open_restaurant(self):
         print('Ресторан открыт к посещению !')
-newRestaurant=Restaurant('Марчеллис','Итальянская')
+newRestaurant=Restaurant('Марчеллис','Итальянская') #экземпляр со своими входными данными
 newRestaurant.describe_restaurant()
 newRestaurant.open_restaurant()
