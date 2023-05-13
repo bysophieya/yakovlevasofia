1d9f134affa808bfa7e5eec26318991cbde72ad1
bysophieya <125361714+bysophieya@users.noreply.github.com>
2023-05-14 00:23:08 +0300

    Update 3.py

--- lab9/3.py
+++ lab9/3.py
@@ -3,8 +3,8 @@ s=0
 with open('products1.csv',newline=" ") as file:
     reader= csv.reader(file) #читаем файл
     print('Нужно купить:')
-    next(reader)
-    for row in reader:
+    next(reader) #возвращает следующую строку объекта чтения
+    for row in reader: #пробегаемся по строкам
         print(row[0] + " - " + row[1] + " шт." + " за " + row[2] + ' руб.')
         s= s+ int(row[1])*int(row[2])
     print('Итоговая сумма:' + str(s) + 'руб.')
