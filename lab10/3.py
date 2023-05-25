da922aaa13b4c6e31fb1f805ead3cad56887d1c7
bysophieya <125361714+bysophieya@users.noreply.github.com>
2023-05-25 12:25:41 +0300

    Update 3.py

--- lab10/3.py
+++ lab10/3.py
@@ -1,28 +1,29 @@
-with open ("en-ru.txt","r+") as file:
-    lr=[]
-    le=[]
-    for i in file:
-        i=i.split('-')
-        s=i[-1]
-        s=s.replace('\n','')
-        lr.append(s)
-lr.sort()
-r=len(le)
-d={}
-le2=[]
-with open ("en-ru.txt","r+") as file3:
-    for i in file3:
-        i=i.replace('\n',"")
-        ir=i.split('-')
-        iru=ir[-1]
-        d.update({iru:i})
-del lr[0]
-print(lr)
-for i in lr:
-    s2=d.get(i)
-    s2=s2.split('-')
-    le2.append(s2[0])
-r=6
-with open ("ru-en.txt","r+") as file2:
-    for i in range(r):
-        file2.write(str(lr[i]) + '-' + str(le2[i]) +'\n')
+filename = 'sl.txt'
+
+
+dict1 = {}
+
+
+with open('sl.txt') as fh:
+    for line in fh:
+
+        command, description = line.strip().split(" - ")
+
+        dict1[command] = description.strip()
+
+out_file = open("test1.json", "w")
+json.dump(dict1, out_file, ensure_ascii=False, indent=4, sort_keys=False)
+out_file.close()
+
+with open('test1.json', encoding='utf-8') as f:
+    data = json.load(f)
+
+new_dict = dict([(value, key) for key, value in data.items()])
+print(new_dict)
+new_d = list(new_dict.keys())
+new_d.sort()
+for i in new_d:
+    f = (i, '-', new_dict[i])
+    e = (' '.join(f))
+    with open ('sl2.txt','a',encoding='utf-8') as file:
+        file.write(e + '\n')
