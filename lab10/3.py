8f648784dcd9332b154433636c37d74109cf55e2
bysophieya <125361714+bysophieya@users.noreply.github.com>
2023-05-25 03:19:56 +0300

    Update 3.py

--- lab10/3.py
+++ lab10/3.py
@@ -1,19 +1,28 @@
-enru={}
-with open('en-ru.txt','r') as file:
-    for line in file:
-        en , ru = line.strip().split(' - ')
-        ru = ru.strip().split(', ')
-        for i in ru:
-            if i not in enru:
-                enru[i]=[en]
-            else:
-                enru[i].append(en)
-en_ru=sorted(enru.items())
-print(enru)
-with open('ru-en.txt',"w") as file:
-    s=""
-    for j in en_ru:
-        s2=f'{j[0]} - {", ".join(i[1])}'
-        s+= s2 + '\n'
-    print(s)
-    file.write(s)
+with open ("en-ru.txt","r+") as file:
+    lr=[]
+    le=[]
+    for i in file:
+        i=i.split('-')
+        s=i[-1]
+        s=s.replace('\n','')
+        lr.append(s)
+lr.sort()
+r=len(le)
+d={}
+le2=[]
+with open ("en-ru.txt","r+") as file3:
+    for i in file3:
+        i=i.replace('\n',"")
+        ir=i.split('-')
+        iru=ir[-1]
+        d.update({iru:i})
+del lr[0]
+print(lr)
+for i in lr:
+    s2=d.get(i)
+    s2=s2.split('-')
+    le2.append(s2[0])
+r=6
+with open ("ru-en.txt","r+") as file2:
+    for i in range(r):
+        file2.write(str(lr[i]) + '-' + str(le2[i]) +'\n')
